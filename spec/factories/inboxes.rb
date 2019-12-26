5ad5678ce1c3bd9edf8cdc0e86e6f5d33e474d5c
Sojan <sojan@pepalo.com>
2019-12-27 04:10:07 +0530

    Spec for widget messages controller

--- spec/factories/inboxes.rb
+++ spec/factories/inboxes.rb
@@ -3,7 +3,11 @@
 FactoryBot.define do
   factory :inbox do
     account
-    association :channel, factory: :channel_widget
+    channel { build(:channel_widget) }
     name { 'Inbox' }
+
+    after(:create) do |inbox|
+      inbox.channel.save!
+    end
   end
 end
